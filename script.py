886dec2da06ab26beb83f24e150f8cfa660dcf8d
nicolas sg <nicolas.sereyjolgmail.com>
2022-09-05 16:09:30 +0200

    maine

--- script.py
+++ script.py
@@ -1,9 +1,9 @@
 import os
 import logging
 
-def main():
+def maine():
     logging.info("The script is now working in the directory: " + os.getcwd())
 
 if __name__ == "__main__":
     print("Hello world!")
-    main()
+    maine()
