6c53e79c121c516af6bb7aa7e9fbb93ed0d95d9a
gitgit <bponcet42@gmail.com>
2022-09-05 16:09:08 +0200

    nom fonction

--- script.py
+++ script.py
@@ -1,9 +1,9 @@
 import os
 import logging
 
-def main():
+def fonction():
     logging.info("The script is now working in the directory: " + os.getcwd())
 
 if __name__ == "__main__":
     print("Hello world!")
-    main()
+    fonction()
