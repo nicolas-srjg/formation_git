98601e1a0ba1421e72611633223fde42cfef4cea
nicolas sg <nicolas.sereyjolgmail.com>
2022-09-05 16:06:10 +0200

    ki

--- script.py
+++ script.py
@@ -1,1 +1,9 @@
-Hello wolrd.
+import os
+import logging
+
+def main():
+    logging.info("The script is now working in the directory: " + os.getcwd())
+
+if __name__ == "__main__":
+    print("Hello world!")
+    main()
